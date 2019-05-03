87f30a5e95e2effa79947898af3ee2b6ec4ac7ab
Yongjae Yi <dididy@knou.ac.kr>
2019-05-03 21:25:12 +0900

    Make some method

--- src/main/java/NumberOfCount.java
+++ src/main/java/NumberOfCount.java
@@ -1,3 +1,18 @@
 public class NumberOfCount {
 
+    private int vowelCount =0;
+    private int consonantCount = 0;
+
+
+    public int getVowelCount(){
+        return vowelCount;
+    }
+
+    public int getConsonantCount(){
+        return consonantCount;
+    }
+
+    public void getNumberOfCount(String str){
+
+    }
 }
