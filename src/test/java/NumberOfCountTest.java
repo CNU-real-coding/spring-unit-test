82c3abbba2939ea16939013d296f5b2d528c652f
ChaeYeon Park <yeoni1098@naver.com>
2019-05-03 21:35:17 +0900

    소문자에서 자음의 개수 확인 메소드 추가

--- src/test/java/NumberOfCountTest.java
+++ src/test/java/NumberOfCountTest.java
@@ -1,3 +1,18 @@
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
 public class NumberOfCountTest {
+
+    @Test
+    public void 소문자에서자음의개수확인(){
+        NumberOfCount numberOfCount = new NumberOfCount();
+        String lowercase = "secret";
+        numberOfCount.getNumberOfCount(lowercase);
+
+        assertThat(numberOfCount.getConsonantCount(),is(4));
+
+    }
 
 }
