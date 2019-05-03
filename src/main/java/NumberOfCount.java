4a382501c76d973b38db60fba4b4af7f8966799c
jihyun <jhhhj1002@hanmail.net>
2019-05-03 21:27:51 +0900

    getNumberOfCount method

--- src/main/java/NumberOfCount.java
+++ src/main/java/NumberOfCount.java
@@ -13,6 +13,22 @@ public class NumberOfCount {
     }
 
     public void getNumberOfCount(String str){
+        String word = str.toLowerCase();
+
+        for(int i=0; i<word.length(); i++){
+            char charAtindex = word.charAt(i);
+            if(charAtindex == 'a' || charAtindex == 'e' || charAtindex == 'i' || charAtindex == 'o' || charAtindex == 'u')
+                vowelCount++;
+            else if('a' <= charAtindex && charAtindex <= 'z' )
+                consonantCount++;
+
+            else
+                continue;
+        }
+
+        System.out.println("자음의 개수" + consonantCount);
+        System.out.println("모음의 개수" + vowelCount);
 
     }
+
 }
